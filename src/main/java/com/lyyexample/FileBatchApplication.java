efbd83791a5b7a5ed7df308f74ebce21f4840bca
yeshen2013 <1486258506@qq.com>
2018-04-23 21:43:34 +0800

    Initial commit

---
+++ src/main/java/com/lyyexample/FileBatchApplication.java
@@ -0,0 +1,12 @@
+package com.lyyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileBatchApplication.class, args);
+	}
+}
